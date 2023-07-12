0287c5c868912b2eb1ff6bcb9ef0293677eaac98
pedrohma07 <pedrohma07@gmail.com>
2023-07-12 11:59:47 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import'./styles/main.css'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import './style.sass'
+
+const index = () => {
+  return (
+    <div className='container w-full'>
+      <header className='container w-full bg-transparent mt-12 h-12 flex justify-around items-center'>
+        <p>test2</p>
+        <p>teste2</p>
+        <p>teste</p>
+      </header>
+      <section className='container w-full'>
+        <div className='titles'>
+        </div>
+          <h1>Great Coffee</h1>
+          <h1 >&#8249;Great Code/&#8250;</h1>
+      </section>
+    </div>
+   
+  )
+}
+
+export default index

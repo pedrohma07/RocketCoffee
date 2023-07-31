ad1dd9953b71f41a64376424b39cfd65dbd4cd55
pedrohma07 <pedrohma07@gmail.com>
2023-07-31 11:32:32 -0300

    feat:

---
+++ src/components/menuMobile/index.jsx
@@ -0,0 +1,15 @@
+import './style.scss'
+
+export const index = () => {
+  return (
+    <div>
+      <div className='navbarMobile flex flex-col'>
+        <a href="" className="item-nav">Home</a>
+        <a href="" className="item-nav">Menu</a>
+        <a href="" className="item-nav">Recompensas</a>
+        <a href="" className="item-nav">Gift Cards</a>
+        <a href="" className="item-nav">Lojas</a>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,32 @@
 import './style.sass'
 import './responsive.css'
+import { useState, useEffect } from 'react';
 
-const index = () => {
+const Index = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+
   return (
     <div className='wrapper container w-full'>
       <header className='container w-full bg-transparent h-12 flex justify-between items-center'>
-        <img src="/assets/logo-desktop.svg" alt="" />
+        <div className='div-logo'>
+          <img src="/assets/logo-desktop.svg" alt="" />
+          {windowWidth <= 430 ? (<img src="/assets/rocketseat.svg" alt="" />) : (<></>)}
+        </div>
+        
         <nav className='navbar flex justify-between'>
           <a href="" className="item-nav">Home</a>
           <a href="" className="item-nav">Menu</a>
@@ -13,17 +34,26 @@ const index = () => {
           <a href="" className="item-nav">Gift Cards</a>
           <a href="" className="item-nav">Lojas</a>
         </nav>
-        <button className='px-8 py-2 rounded-lg'>PEGAR MEU CAFÉ</button>
+        <button className='header-btn px-8 py-2 rounded-lg'>PEGAR MEU CAFÉ</button>
       </header>
       <section className='container w-full'>
-        <div className='titles'>
-          <h1>Great Coffee</h1>
-          <h1 >&#8249;Great Code/&#8250;</h1>
-        </div>
+        {windowWidth <= 430 ? (
+          <div className='titles'>
+            <h2 className={' text-3xl'}>O café que fará seu código decolar para o próximo nível.</h2>
+            <button className='px-8 py-2 rounded-lg'>PEGAR MEU CAFÉ</button>
+            <h1 className={' text-5xl'}>Great Coffee</h1>
+            <h1 className={' text-5xl'}>&#8249;Great Code/&#8250;</h1>
+          </div>
+          ) : (
+          <div className='titles'>
+            <h1 className={' text-8xl'}>Great Coffee</h1>
+            <h1 className={' text-8xl'}>&#8249;Great Code/&#8250;</h1>
+          </div>
+        )}
       </section>
     </div>
    
   )
 }
 
-export default index
+export default Index
